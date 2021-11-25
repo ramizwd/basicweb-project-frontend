069e55a29b687092f5fd7f1309d44876ee7aa08d
leinonenko <Kabaman1995p>
2021-11-25 15:39:47 +0200

    login and registration js parts

--- ui/scripts/loginPage.js
+++ ui/scripts/loginPage.js
@@ -1,2 +1,28 @@
 'use strict';
 console.log('login page');
+
+const url = 'http://10.114.32.27/';
+const login = document.querySelector('#login');
+
+login.addEventListener('submit', async (evt) => {
+  evt.preventDefault();
+  const data = serializeJson(login);
+  const fetchOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  };
+  const response = await fetch(url + '/auth/login', fetchOptions);
+  const json = await response.json();
+  console.log('login response', json);
+  if (!json.user) {
+    alert(json.message);
+  } else {
+    // token
+    sessionStorage.setItem('token', json.token);
+    sessionStorage.setItem('user', JSON.stringify(json.user));
+    location.href = 'frontPage.html';
+  }
+});
