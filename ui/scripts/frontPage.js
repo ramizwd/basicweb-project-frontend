bfc84540571e7c93f56746e53658fd007e857815
R <63073421+ramizwd@users.noreply.github.com>
2021-11-30 22:45:42 +0200

    fixed indentation/cleaned code, added poster nickname and pfp

--- ui/scripts/frontPage.js
+++ ui/scripts/frontPage.js
@@ -4,72 +4,61 @@ const url = 'http://localhost:3000';
 
 // Function to fetch data for users
 const getUserInfo = async (id) => {
-  const response = await fetch(url + '/user/' + id);
-  const user = await response.json();
-  return user;
+    const response = await fetch(url + '/user/' + id);
+    const user = await response.json();
+    return user;
 };
 
 // Function to fetch data for posts
 const getPostInfo = async () => {
-  try {
-    const response = await fetch(url + '/post');
-    const post = await response.json();
-    createPosts(post);
-  } catch (e) {
-    console.log(e.message);
-  }
+    try {
+        const response = await fetch(url + '/post');
+        const post = await response.json();
+        createPosts(post);
+    } catch (e) {
+        console.log(e.message);
+    }
 };
 getPostInfo();
 
 const feed = document.querySelector('#postFeed');
 const nickname = document.querySelector('#nickname');
-// if statement to check if logged in to replace nickname for button "login"
-//nickname.innerHTML = getUserInfo().user.name;
 const profileImg = document.querySelector('.profileImg');
-//profileImg.innerHTML = getUserInfo().user.image;                                // image not implemented yet
+//profileImg.innerHTML = getUserInfo().user.image;  // image not implemented yet
 
 // Function for creating post containers
 const createPosts = (posts) => {
-  // clear ul
-  feed.innerHTML = '';
-  // Creating HTML elements
-  posts.forEach((post) => {
-    const userPost = document.createElement('li');
+    // clear ul
+    feed.innerHTML = '';
+    // Creating HTML elements
+    posts.forEach((post) => {
+        const userPost = document.createElement('li');
 
-    //if the there is not photo do the layout like this
-    if (post.filename == null) {
-      const postTitle = document.createElement('h3');
-      postTitle.innerHTML = `${post.title}`;
-      const buildText = document.createElement('p');
-      buildText.innerHTML = `${post.description}`;
-    } else {
-      //if not null do the layout like this
-      const postImg = document.createElement('img');
-      postImg.src = url + '/' + post.filename;  // will be changes to filename
-      postImg.alt = post.title;
-      //const userImg = document.createElement('img');
-      //const postNickname = document.createElement('h5');
-      const postTitle = document.createElement('h3');
-      postTitle.innerHTML = `${post.title}`;
-      //const buildList = document.createElement('ul');
-      //const buildPart = document.createElement('li');
-      const buildText = document.createElement('p');
-      buildText.innerHTML = `${post.description}`;
-    }
+        const buildText = document.createElement('p');
+        buildText.innerHTML = `${post.description}`;
 
-// Added attributes for easier use in CSS
-    // buildList.setAttribute('class', 'buildList');
-    // buildPart.setAttribute('class', 'buildPart');
+        //if not null do the layout like this
+        const postImg = document.createElement('img');
+        postImg.src = url + '/' + post.filename; // will be changes to filename
+        postImg.alt = post.title;
+        const userImg = document.createElement('img');
+        //const postNickname = document.createElement('h5');
+        const postTitle = document.createElement('h3');
+        postTitle.innerHTML = `${post.title}`;
+        // Poster nickname
+        const poster = document.createElement('h1');
+        poster.innerHTML = `Poster: ${post.postername}`;
+        // Poster profile picture
+        const posterPfp = document.createElement('img');
+        posterPfp.src = url + '/' + post.profile_picture;
 
-// Placing the hierarchy in the post object
-    feed.appendChild(userPost);
-    //userPost.appendChild(userImg);
-    //userPost.appendChild(postNickname);
-    userPost.appendChild(postImg);
-    userPost.appendChild(postTitle);
-    //userPost.appendChild(buildList);
-    //buildList.appendChild(buildPart);
-    userPost.appendChild(buildText);
-
-  });
+        // Placing the hierarchy in the post object
+        feed.appendChild(userPost);
+        userPost.appendChild(posterPfp);
+        userPost.appendChild(poster);
+        userPost.appendChild(userImg);
+        userPost.appendChild(postImg);
+        userPost.appendChild(postTitle);
+        userPost.appendChild(buildText);
+    });
 };
