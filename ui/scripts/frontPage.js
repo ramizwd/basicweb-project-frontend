0c8f15e564f78e4885a1b8ea99f957ede3c0da28
R <63073421+ramizwd@users.noreply.github.com>
2021-12-02 17:12:27 +0200

    add new post functionality and style, cleaned front html

--- ui/scripts/frontPage.js
+++ ui/scripts/frontPage.js
@@ -1,4 +1,5 @@
 'use strict';
+
 console.log('front page');
 const url = 'http://localhost:3000';
 
@@ -38,12 +39,12 @@ const createPosts = (posts) => {
         buildText.innerHTML = `${post.description}`;
 
         //if not null do the layout like this
-      if (post.filename != null) {
-        const postImg = document.createElement('img');
-        postImg.src = url + '/' + post.filename;  // will be changes to filename
-        postImg.alt = "404 image not found";
-        userPost.appendChild(postImg);
-      }
+        if (post.filename != null) {
+            const postImg = document.createElement('img');
+            postImg.src = url + '/' + post.filename; // will be changes to filename
+            postImg.alt = '404 image not found';
+            userPost.appendChild(postImg);
+        }
         const userImg = document.createElement('img');
         //const postNickname = document.createElement('h5');
         const postTitle = document.createElement('h3');
@@ -54,7 +55,7 @@ const createPosts = (posts) => {
         // Poster profile picture
         const posterPfp = document.createElement('img');
         posterPfp.src = url + '/' + post.profile_picture;
-      console.log(post.profile_picture);
+        console.log(post.profile_picture);
 
         // Placing the hierarchy in the post object
         feed.appendChild(userPost);
