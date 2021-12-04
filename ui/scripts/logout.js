fe39e70c4112de5623d87feeeeb27082716f4ba4
R <63073421+ramizwd@users.noreply.github.com>
2021-12-04 22:15:35 +0200

    fix: code indendation

--- ui/scripts/logout.js
+++ ui/scripts/logout.js
@@ -2,16 +2,16 @@
 const url = 'http://localhost:3000'; // change url when uploading to server
 
 (async () => {
- try {
-  const response = await fetch(url + '/auth/logout');
-  const json = await response.json();
-  console.log(json);
-  // remove token
-  sessionStorage.removeItem('token');
-  sessionStorage.removeItem('user');
-  alert('You have logged out');
-  location.href = 'loginPage.html';
- } catch (e) {
-  console.log(e.message);
- }
+    try {
+        const response = await fetch(url + '/auth/logout');
+        const json = await response.json();
+        console.log(json);
+        // remove token
+        sessionStorage.removeItem('token');
+        sessionStorage.removeItem('user');
+        alert('You have logged out');
+        location.href = 'loginPage.html';
+    } catch (e) {
+        console.log(e.message);
+    }
 })();
--- ui/scripts/registerPage.js
+++ ui/scripts/registerPage.js
@@ -6,16 +6,16 @@ const registration = document.querySelector('#register');
 
 // registration
 registration.addEventListener('submit', async (evt) => {
- evt.preventDefault();
- const data = serializeJson(registration);
- const fetchOptions = {
-  method: 'POST',
-  headers: {
-   'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(data),
- };
- const response = await fetch(url + '/auth/register', fetchOptions);
- const json = await response.json();
- alert(json.message);
+    evt.preventDefault();
+    const data = serializeJson(registration);
+    const fetchOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    };
+    const response = await fetch(url + '/auth/register', fetchOptions);
+    const json = await response.json();
+    alert(json.message);
 });
