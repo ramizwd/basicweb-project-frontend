71d6b3619b69a5bbbbf310665cf48697d8423775
leinonenko <Kabaman1995p>
2021-12-04 18:39:50 +0200

    Reformatted code, fixed issue with extra spaces in webstorm and added comments to loginPage

--- ui/scripts/addPost.js
+++ ui/scripts/addPost.js
@@ -9,17 +9,17 @@ const span = document.getElementsByClassName('close-modal')[0];
 
 // display modal when the add new post button clicked
 postBtn.onclick = () => {
-    modal.style.display = 'block';
+ modal.style.display = 'block';
 };
 // Hide modal when the span is clicked
 span.onclick = () => {
-    modal.style.display = 'none';
+ modal.style.display = 'none';
 };
 // Hide modal when the user click outside of it
 window.onclick = (evt) => {
-    if (evt.target == modal) {
-        modal.style.display = 'none';
-    }
+ if (evt.target == modal) {
+  modal.style.display = 'none';
+ }
 };
 
 // Get html elements
@@ -27,21 +27,21 @@ const addPost = document.querySelector('#addPost');
 
 // Add new post
 addPost.addEventListener('submit', async (evt) => {
-    evt.preventDefault(); // stop default action if event is not handled
-    const postBody = new FormData(addPost); // format the values for post method content
-    const fetchOptions = {
-        method: 'POST',
-        headers: {
-            Authorization: 'Bearer ' + sessionStorage.getItem('token'),
-        },
-        body: postBody,
-    };
-    // send post method and the values to the post route.
-    // get a json response and display it as successfully added post message as an alert
-    const res = await fetch(url + '/post', fetchOptions);
-    const json = await res.json();
-    alert(json.message);
-    location.href = 'frontPage.html';
+ evt.preventDefault(); // stop default action if event is not handled
+ const postBody = new FormData(addPost); // format the values for post method content
+ const fetchOptions = {
+  method: 'POST',
+  headers: {
+   Authorization: 'Bearer ' + sessionStorage.getItem('token'),
+  },
+  body: postBody,
+ };
+ // send post method and the values to the post route.
+ // get a json response and display it as successfully added post message as an alert
+ const res = await fetch(url + '/post', fetchOptions);
+ const json = await res.json();
+ alert(json.message);
+ location.href = 'frontPage.html';
 });
 
 // BUG - snackbar shows when post is not added
