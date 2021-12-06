b7741bc02a15282450eb98786ebafe750a8b7afd
leinonenko <Kabaman1995p>
2021-12-06 21:36:52 +0200

    Fixed getting data of user throught user_id

--- ui/scripts/profilePage.js
+++ ui/scripts/profilePage.js
@@ -1,17 +1,55 @@
 'use strict';
 console.log('profile page');
 const url = 'http://localhost:3000';
+// get user data for admin check
+const user = JSON.parse(sessionStorage.getItem('user'));
 
 // Function to fetch data for users
+const getUserInfo = async () => {
+  try {
+    const fetchOptions = {
+      headers: {
+        Authorization: 'Bearer ' + sessionStorage.getItem('token'),
+      },
+    };
+    const res = await fetch(url + '/user/' + user.user_id, fetchOptions);
+    const users = await res.json();
+    console.log('user', users)
+    createBio(users);
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+getUserInfo();
+/*
 const getUserInfo = async (id) => {
   const response = await fetch(url + '/user/' + '1');
-  const user = await response.json();
+  const users = await response.json();
   console.log('user', user);
   createBio(user[0]);
 };
 getUserInfo();
+ */
 
 // Function to fetch data for posts
+const getPostInfo = async () => {
+  try {
+    const fetchOptions = {
+      headers: {
+        Authorization: 'Bearer ' + sessionStorage.getItem('token'),
+      },
+    };
+    const res = await fetch(url + '/post', fetchOptions);
+    const posts = await res.json();
+    createPosts(posts);
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+
+getPostInfo();
+
+/*
 const getPostInfo = async () => {
   try {
     const response = await fetch(url + '/post');
@@ -22,6 +60,7 @@ const getPostInfo = async () => {
   }
 };
 getPostInfo();
+*/
 
 // Selection of existing html elements
 const bio = document.querySelector('.bio');
@@ -61,50 +100,51 @@ const createPosts = (posts) => {
   // Clear ul
   postList.innerHTML = '';
   // Creating HTML elements
-  posts.forEach((post) => {
-    const userPost = document.createElement('li');
-    postList.appendChild(userPost);
+      posts.forEach((post) => {
+      const userPost = document.createElement('li');
+      postList.appendChild(userPost);
 
-    // Poster profile picture
-    const posterPfp = document.createElement('img');
-    posterPfp.src = url + '/' + post.profile_picture;
-    console.log(post.profile_picture);
+      // Poster profile picture
+      const posterPfp = document.createElement('img');
+      posterPfp.src = url + '/' + post.profile_picture;
+      console.log(post.profile_picture);
 
-    // Poster nickname
-    const poster = document.createElement('h5');
-    poster.innerHTML = `Poster: ${post.postername}`;
+      // Poster nickname
+      const poster = document.createElement('h5');
+      poster.innerHTML = `Poster: ${post.postername}`;
 
-    const postsHeader = document.createElement('div');
-    userPost.appendChild(postsHeader)
-    postsHeader.appendChild(posterPfp);
-    postsHeader.appendChild(poster);
+      const postsHeader = document.createElement('div');
+      userPost.appendChild(postsHeader)
+      postsHeader.appendChild(posterPfp);
+      postsHeader.appendChild(poster);
 
-    //Posts image (if not null do the layout like this)
-    if (post.filename != null) {
-      const postImg = document.createElement('img');
-      postImg.src = url + '/' + post.filename;  // will be changes to filename
-      postImg.alt = '404 image not found';
-      userPost.appendChild(postImg);
-      postImg.setAttribute('id', 'postImage');
-    }
-    // Title
-    const postTitle = document.createElement('h3');
-    postTitle.innerHTML = `${post.title}`;
-    userPost.appendChild(postTitle);
+      //Posts image (if not null do the layout like this)
+      if (post.filename != null) {
+        const postImg = document.createElement('img');
+        postImg.src = url + '/' + post.filename;  // will be changes to filename
+        postImg.alt = '404 image not found';
+        userPost.appendChild(postImg);
+        postImg.setAttribute('id', 'postImage');
+      }
+      // Title
+      const postTitle = document.createElement('h3');
+      postTitle.innerHTML = `${post.title}`;
+      userPost.appendChild(postTitle);
 
-    // Build
-    const buildText = document.createElement('p');
-    buildText.innerHTML = `${post.description}`;
-    userPost.appendChild(buildText);
+      // Build
+      const buildText = document.createElement('p');
+      buildText.innerHTML = `${post.description}`;
+      userPost.appendChild(buildText);
 
 
-    // Placing the hierarchy in the post object
+      // Placing the hierarchy in the post object
 
-    // Setting attributes for repeating elements
-    posterPfp.setAttribute('id', 'posterAvatar');
-    buildText.setAttribute('id', 'postText');
-    postsHeader.setAttribute('id', 'postsHeader');
-  });
+      // Setting attributes for repeating elements
+      posterPfp.setAttribute('id', 'posterAvatar');
+      buildText.setAttribute('id', 'postText');
+      postsHeader.setAttribute('id', 'postsHeader');
+    });
+
 };
 
 
