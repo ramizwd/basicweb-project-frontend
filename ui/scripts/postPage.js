133c59edcc5e5c0d0abef15f3f7bcfd2f7f98e62
leinonenko <Kabaman1995p>
2021-12-09 18:52:55 +0200

    Added function to create and fill post info. Still need to fix

--- ui/scripts/postPage.js
+++ ui/scripts/postPage.js
@@ -2,38 +2,86 @@
 
 const url = 'http://localhost:3000';
 const main = document.querySelector('main');
+// get user and post data for admin check
+//const user = JSON.parse(sessionStorage.getItem('user'));
+//const post = JSON.parse(sessionStorage.getItem('post'));
 
-//Creating html elements for post
-const postContainer = document.createElement('div');
-postContainer.setAttribute('id', 'postContainer');
-const userAvatar = document.createElement('img');
-userAvatar.setAttribute('id', 'avatar');
-const userNickname = document.createElement('h2');
-const postImg = document.createElement('img');
-postImg.setAttribute('id', 'postImg');
-const postTitle = document.createElement('h1');
-const postText = document.createElement('p');
-postText.setAttribute('id', 'description');
-const postBuild = document.createElement('p');
-postText.setAttribute('id', 'builtList');
-const builtListTitle = document.createElement('h3');
-builtListTitle.innerHTML = 'PC hardware list';
+// Function to fetch data for post
+const getPost = async () => {
+    try {
+        const fetchOptions = {
+            headers: {
+                Authorization: 'Bearer ' + sessionStorage.getItem('token'),
+            },
+        };
+        const res = await fetch(url + '/post/' + post.post_id, fetchOptions);
+        const post = await res.json();
+        console.log(post);
+        createPost(post);
+    } catch (e) {
+        console.log(e.message);
+    }
+};
+getPost();
+
+/*
+const getPost = async (id) => {
+    const response = await fetch(url + '/post/' + '12');
+    const post = await response.json();
+    console.log('post', post)
+    return post;
+};
+ */
+
+const createPost = (post) => {
+    //Creating html elements for post
+    const postContainer = document.createElement('div');
+    postContainer.setAttribute('id', 'postContainer');
+    postContainer.setAttribute('class', 'left');
+    postContainer.innerHTML = '';
+
+    const userAvatar = document.createElement('img');
+    userAvatar.setAttribute('id', 'avatar');
+    const userNickname = document.createElement('h2');
+
+
+    if (posts.filename != null) {
+        const postImg = document.createElement('img');
+        postImg.src = url + '/' + post.filename; // will be changes to filename
+        postImg.alt = '404 image not found';
+        postImg.setAttribute('id', 'postImg');    }
+
+    const postTitle = document.createElement('h1');
+    postTitle.innerHTML = `${post.title}`;
+
+    const postText = document.createElement('p');
+    postText.setAttribute('id', 'description');
+    postText.innerHTML = `${post.description}`;
+
+    const postBuild = document.createElement('p');
+    postText.setAttribute('id', 'builtList');
+
+    const builtListTitle = document.createElement('h3');
+    builtListTitle.innerHTML = 'PC hardware list';
 
 // Placing the hierarchy in the post container part
-main.appendChild(postContainer);
-postContainer.appendChild(userAvatar);
-postContainer.appendChild(userNickname);
-postContainer.appendChild(postImg);
-postContainer.appendChild(postTitle);
-postContainer.appendChild(postText);
-postContainer.appendChild(postBuild);
-postContainer.appendChild(builtListTitle);
+    main.appendChild(postContainer);
+    postContainer.appendChild(userAvatar);
+    postContainer.appendChild(userNickname);
+    postContainer.appendChild(postImg);
+    postContainer.appendChild(postTitle);
+    postContainer.appendChild(postText);
+    postContainer.appendChild(postBuild);
+    postContainer.appendChild(builtListTitle);
+};
+
 
 
 
 //Creating html elements for comments
 const commentContainer = document.createElement('div');
 commentContainer.setAttribute('id', 'commentContainer');
+commentContainer.setAttribute('class', 'right');
 const containerTitle = document.createElement('h3');
 commentContainer.innerHTML = 'Write a comment';
 
