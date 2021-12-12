4d0b1d62fd612514bc7381af7a07690c58c5dcbe
leinonenko <Kabaman1995p>
2021-12-12 13:14:53 +0200

    Edited positioning of objects in the post, added votes, edited CSS

--- scripts/postPage.js
+++ scripts/postPage.js
@@ -23,6 +23,7 @@ const createPost = (posts) => {
     postContainer.setAttribute('class', 'item');
     postContainer.innerHTML = '';
 
+    //Users avatar
     const userAvatar = document.createElement('img');
     userAvatar.setAttribute('id', 'avatar');
     //getting the default profile pic if not yet set
@@ -30,30 +31,50 @@ const createPost = (posts) => {
     'placeholder/male-default-placeholder-avatar-profile-260nw-582509551.jpg';
     userAvatar.width = '45';
     userAvatar.height = '45';
+    // Username of poster
     const userNickname = document.createElement('h2');
+    userNickname.innerHTML = `${posts.postername}`;
 
+    //Title
     const postTitle = document.createElement('h1');
     postTitle.innerHTML = `${posts.title}`;
 
+    //Description of the build
     const postText = document.createElement('p');
     postText.setAttribute('id', 'description');
     postText.innerHTML = `${posts.description}`;
 
+    // Text with a specific list of build equipment
     const postBuild = document.createElement('p');
     postText.setAttribute('id', 'builtList');
 
     const builtListTitle = document.createElement('h3');
     builtListTitle.innerHTML = 'PC hardware list';
 
+    // Header with avatar and nickname
+    const postHeader = document.createElement('div');
+    postHeader.setAttribute('id', 'postHeader');
+
+    // Upvote button
+    const upVote = document.createElement('button');
+    upVote.innerHTML = 'Upvote';
+    // Downvote button
+    const downVote = document.createElement('button');
+    downVote.innerHTML = 'Dowvote';
+    // Total votes button
+    const votes = document.createElement('p');
+    votes.innerHTML = `${posts.votes}`;
+    const votesContainer = document.createElement('div');
+    votesContainer.setAttribute('id', 'votesContainer');
+
+    const imgCont = document.createElement('figure');
+
+
 // Placing the hierarchy in the post container part
     main.appendChild(postContainer);
-    postContainer.appendChild(userAvatar);
-    postContainer.appendChild(userNickname);
-
-    postContainer.appendChild(postTitle);
-    postContainer.appendChild(postText);
-    postContainer.appendChild(postBuild);
-    postContainer.appendChild(builtListTitle);
+    postContainer.appendChild(postHeader);
+    postHeader.appendChild(userAvatar);
+    postHeader.appendChild(userNickname);
 
     //if not null do the layout like this
     if (posts.filename != null) {
@@ -67,13 +88,14 @@ const createPost = (posts) => {
         ) {
             //create img elements
             postImg = document.createElement('img');
+            postImg.setAttribute('id', 'postImg');
             //source where to get it
             postImg.src = url + '/thumbnails/' + posts.filename; // will be changes to filename
             postImg.style.width = '100%';
             //if no img alternative
             postImg.alt = '404 image not found';
             //append postIMG
-            postContainer.appendChild(postImg);
+            imgCont.appendChild(postImg);
         } else if (posts.file_type === 'video/mp4') {
             //create video element
             postImg = document.createElement('video');
@@ -86,9 +108,20 @@ const createPost = (posts) => {
             //if no img alternative
             postImg.alt = '404 image not found';
             //append postIMG
-            postContainer.appendChild(postImg);
+            imgCont.appendChild(postImg);
         }
     }
+
+    postContainer.appendChild(imgCont);
+
+    postContainer.appendChild(postTitle);
+    postContainer.appendChild(postText);
+    postContainer.appendChild(votesContainer);
+    votesContainer.appendChild(upVote);
+    votesContainer.appendChild(downVote);
+    votesContainer.appendChild(votes);
+    postContainer.appendChild(postBuild);
+    postContainer.appendChild(builtListTitle);
 
 };
 
@@ -113,7 +146,9 @@ btnPost.innerHTML = 'Post';
 const commentList = document.createElement('ul');
 const comment = document.createElement('li');
 const commentNickname = document.createElement('h5');
+commentNickname.innerHTML = 'Username';     // Will be added after backend
 const commentText = document.createElement('p');
+commentText.innerHTML = 'Great build! Love it even it´s weird..'     // Will be added after backend
 commentText.setAttribute('id', 'commentText');
 
 // Placing the hierarchy in the post comment part
