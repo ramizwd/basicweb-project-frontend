3258643eeba973bdb27fe4dc754609e28d287ebd
Johnkai Cortez <73104201+Johnkai2196@users.noreply.github.com>
2021-12-14 23:40:35 +0200

    Work in progress not yet done

--- scripts/settingUser.js
+++ scripts/settingUser.js
@@ -6,16 +6,16 @@ const setting = document.querySelector('#setting');
 const settingbtn = document.querySelector('#setting-btn');
 const spanSetting = document.getElementsByClassName('close-modal-setting')[0];
 
-let pro
+let pro;
 // display modal when the add new post button clicked
 setting.onclick = () => {
-    pro=usersLog
-    x()
+    pro = usersLog;
+    x();
     modalSetting.style.display = 'block';
 };
 settingbtn.onclick = () => {
-    pro=users
-    x()
+    pro = users;
+    x();
     modalSetting.style.display = 'block';
 };
 
@@ -24,58 +24,69 @@ spanSetting.onclick = () => {
     modalSetting.style.display = 'none';
 };
 const x = () => {
-    const role=document.getElementById("#role")
 
-if (pro.role === 1) {
-    if(!!document.getElementById('settingUser').
-    getElementsByClassName('form-input')[3]){
+    if (usersLog.role === 1) {
+        if (!!document.getElementById('settingUser').
+        getElementsByClassName('form-input')[3]) {
+            document.getElementById('settingUser').
+            getElementsByClassName('form-input')[3].remove();
+        }
+
+    } else {
+        if (!!document.getElementById('settingUser').
+        getElementsByClassName('form-input')[3] === false) {
+            const role = document.getElementById('role');
+            const roleBar = document.createElement('input');
+            roleBar.className = 'form-input';
+            roleBar.type = 'number';
+            roleBar.min = '0';
+            roleBar.max = '1';
+            roleBar.required=true
+            role.append(roleBar);
+
+        }
+        document.getElementById('settingUser').
+        getElementsByClassName('form-input')[3].value = pro.role;
+    }
     document.getElementById('settingUser').
-    getElementsByClassName('form-input')[3].remove();}
-
-} else {
-    console.log(role.contains())
+    getElementsByClassName('form-input')[0].value = pro.username;
     document.getElementById('settingUser').
-    getElementsByClassName('form-input')[3].value = pro.role;
-}
-document.getElementById('settingUser').
-getElementsByClassName('form-input')[0].value = pro.username;
-document.getElementById('settingUser').
-getElementsByClassName('form-input')[1].value = pro.email;
-
+    getElementsByClassName('form-input')[1].value = pro.email;
 
 // Get html elements
-const setting = document.querySelector('#settingUser');
+    const setting = document.querySelector('#settingUser');
 
-setting.addEventListener('submit', async (evt) => {
-    evt.preventDefault(); // stop default action if event is not handled
-    const data = serializeJson(setting);
-    console.log(pro);
-    data.id = pro.user_id;
-    console.log('user id', pro.user_id);
-    console.log(data);
+    setting.addEventListener('submit', async (evt) => {
+        evt.preventDefault(); // stop default action if event is not handled
+        const data = serializeJson(setting);
+        console.log(pro);
+        data.id = pro.user_id;
+        console.log('user id', pro.user_id);
+        console.log(data);
 
-    const fetchOptions = {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: 'Bearer ' + sessionStorage.getItem('token'),
-        },
-        body: JSON.stringify(data), // body data type must match "Content-Type" header
-    };
-    // send put method and the values to the put route.
-    // get a json response and display it as successfully added post message as an alert
+        const fetchOptions = {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer ' + sessionStorage.getItem('token'),
+            },
+            body: JSON.stringify(data), // body data type must match "Content-Type" header
+        };
+        // send put method and the values to the put route.
+        // get a json response and display it as successfully added post message as an alert
 
-    // get user data for admin check
-    //const user = JSON.parse(sessionStorage.getItem('user'));
-    const response = await fetch(
-    url + '/user',
-    fetchOptions,
-    );
-    const json = await response.json();
-    if (json.error) {
-        alert(json.error.message);
-    } else {
-        alert(json.message);
-    }
-    location.href = 'profilePage.html';
-});}
+        // get user data for admin check
+        //const user = JSON.parse(sessionStorage.getItem('user'));
+        const response = await fetch(
+        url + '/user',
+        fetchOptions,
+        );
+        const json = await response.json();
+        if (json.error) {
+            alert(json.error.message);
+        } else {
+            alert(json.message);
+        }
+        location.href = 'profilePage.html';
+    });
+};
