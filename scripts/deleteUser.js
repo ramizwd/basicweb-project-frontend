ce899fef7fc877b97215f18bc6fc5f15c20efc65
Johnkai Cortez <73104201+Johnkai2196@users.noreply.github.com>
2021-12-15 20:54:46 +0200

    work in progres test delete still

---
+++ scripts/deleteUser.js
@@ -0,0 +1,27 @@
+'use strict';
+//get the html
+const deletebtn = document.getElementById('delete-btn');
+const deleteFun = () => {
+    //when button click
+  deletebtn.addEventListener('click', async() => {
+        const fetchOptions = {
+            method: 'DELETE',
+            headers: {
+                Authorization: 'Bearer ' + sessionStorage.getItem('token'),
+            },
+        };
+      // send delete method
+      // get a json response and then redirect to login-page
+        try {
+            const response = await fetch(
+            url + '/user/' + current.user_id,
+            fetchOptions,
+            );
+            const json = await response.json();
+            console.log('delete response', json);
+            location.href = locationpl;
+        } catch (e) {
+            console.log(e.message);
+        }
+    });
+}
